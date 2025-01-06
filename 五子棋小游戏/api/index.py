3c2a0f4b53d2ef95363708469cdbcee0bd2ae1ce
xitingwang <9883790+xiitngwang@user.noreply.gitee.com>
2025-01-06 18:03:01 +0800

    Update Flask configuration for Vercel

--- 五子棋小游戏/api/index.py
+++ 五子棋小游戏/api/index.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    template_folder=os.path.abspath("api/templates"),
+    static_folder=os.path.abspath("api/static"))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-app.debug = False
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
